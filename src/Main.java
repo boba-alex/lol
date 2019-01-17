2d4a2e1b37563f576d375a0bdebe3c7baefc3dfc
apolishchuk <apolishchuk@elinext.com>
2019-01-17 18:40:37 +0300

    new back

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("New commit 2 !!!");
+		System.out.println("New commit !!!");
 		System.out.println("New new commit!");
 	}
 
